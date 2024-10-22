213c2c9ce588087ce6740d5706cea9803ee6a10e
Haojin Yang <haojin.yang@.hpi.uni-potsdam.de>
2024-10-22 14:43:35 +0200

    Fixed issue in serve demo.

--- gbx_lm/fastapi_server.py
+++ gbx_lm/fastapi_server.py
@@ -373,4 +373,4 @@ def generate_chat_completion(prompt, request, model, tokenizer):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
