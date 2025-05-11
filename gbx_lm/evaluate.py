25d358ce49accdb29ae27e31de6184c382e60c99
GBAI Admin <gbai_admin@GBAIs-Mac-Studio.local>
2025-05-12 00:18:25 +0200

    small fix in evaluate script. only gbx qwen3 model needs converting mask to bf16 dtype.

--- gbx_lm/evaluate.py
+++ gbx_lm/evaluate.py
@@ -161,7 +161,8 @@ class MLXLM(LM):
 
             offset = cache[0].offset
             mask = create_causal_mask(T, offset, lengths=lengths)
-            if self.is_qwen3:
+            if self.is_qwen3 and self.model_type == "gbx":
+                print("GBX Qwen3, converting mask to bf16 dtype.")
                 mask = mask.astype(mx.bfloat16)
             logits = self._model(inp, cache=cache, mask=mask)
             log_probs = nn.log_softmax(logits.astype(mx.float32))
